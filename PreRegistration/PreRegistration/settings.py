0b7554918f33d72ba6b16675600559dcf4ec3174
EmreKc <emreh134150@gmail.com>
2019-03-17 16:12:08 +0300

    cors-header

--- PreRegistration/PreRegistration/settings.py
+++ PreRegistration/PreRegistration/settings.py
@@ -35,6 +35,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders',
     'rest_framework',
     'Courses'
 ]
@@ -47,6 +48,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
 ]
 
 ROOT_URLCONF = 'PreRegistration.urls'
