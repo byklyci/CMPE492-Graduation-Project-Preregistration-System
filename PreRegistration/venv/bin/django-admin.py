1474c1766812b5a9cffd4f43027b8108911d8938
EmreKc <emreh134150@gmail.com>
2019-03-17 16:23:37 +0300

    PreRegistration deleted

--- PreRegistration/venv/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/Users/bekirburakaslan/Desktop/GitHub/CMPE492-Graduation-Project-Preregistration-System/PreRegistration/venv/bin/python3
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
